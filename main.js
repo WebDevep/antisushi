85639717afdd153feb0045dff673fdc1a3f1df4f
WebDevep <webdevep28@gmail.com>
2019-02-13 16:34:08 +0300

    preloader

--- main.js
+++ main.js
@@ -1810,6 +1810,7 @@ $(document).ready(function () {
 
 $(window).on('load',function() {
 	getcheckoutbasket();
+	$('#preloader').hide();	
 })
 $(document).ready(function () {
 	$('li.dropdown').hover(function() {
